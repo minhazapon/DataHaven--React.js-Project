5a48d2d125d24c03ef5326863daf4568c8aae213
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-19 08:11:28 -0800

    added file

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,6 +1,15 @@
+import { NavLink } from "react-router-dom"
 
 
 function Nav() {
+
+    const NavZ = <>
+        <li className="text-xs font-thin"><NavLink to='/'>Home</NavLink></li>
+        <li className="text-xs font-thin"><NavLink to='/add'>Add Information</NavLink></li>
+        <li className="text-xs font-thin"><NavLink to='/service'>Service</NavLink></li>
+        <li className="text-xs font-thin"><NavLink to='/contact'>Contact</NavLink></li>
+    </>
+
     return (
         <div>
             <div className="navbar bg-base-100 shadow p-5">
@@ -23,15 +32,7 @@ function Nav() {
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            {NavZ}
                         </ul>
                     </div>
                     <div className=" flex items-center gap-2 ">
@@ -41,22 +42,12 @@ function Nav() {
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {NavZ}
                     </ul>
                 </div>
-                <div className="navbar-end">
+                {/* <div className="navbar-end">
                     <a className="btn">Button</a>
-                </div>
+                </div> */}
             </div>
         </div>
     )
