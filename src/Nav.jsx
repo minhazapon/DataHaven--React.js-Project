92f29b095ba615df5bd15e75cc112b6e860b3ec0
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-20 06:06:18 -0800

    added file

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -15,41 +15,54 @@ function Nav() {
             <div className="navbar bg-base-100 shadow p-5">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <button
+                            tabIndex={0}
+                            className="btn btn-ghost lg:hidden"
+                        >
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
+                                className="h-6 w-6"
                                 fill="none"
                                 viewBox="0 0 24 24"
-                                stroke="currentColor">
+                                stroke="currentColor"
+                            >
                                 <path
                                     strokeLinecap="round"
                                     strokeLinejoin="round"
                                     strokeWidth="2"
-                                    d="M4 6h16M4 12h8m-8 6h16" />
+                                    d="M4 6h16M4 12h8m-8 6h16"
+                                />
                             </svg>
-                        </div>
+                        </button>
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
+                        >
                             {NavZ}
                         </ul>
                     </div>
-                    <div className=" flex items-center gap-2 ">
-                        <img className="h-[50px]" src="https://cdn-icons-png.flaticon.com/128/5337/5337412.png" alt="" />
-                        <a className="text-4xl dataText">DataHaven</a>
+                    <div className="flex items-center gap-2 ml-4 lg:ml-16">
+                        <img
+                            className="h-[40px] lg:h-[50px]"
+                            src="https://cdn-icons-png.flaticon.com/128/5337/5337412.png"
+                            alt="Logo"
+                        />
+                        <a className="text-2xl lg:text-4xl font-semibold dataText">
+                            DataHaven
+                        </a>
                     </div>
                 </div>
+
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        {NavZ}
-                    </ul>
+                    <ul className="menu menu-horizontal px-1">{NavZ}</ul>
                 </div>
-                {/* <div className="navbar-end">
+
+                <div className="navbar-end mr-4 lg:mr-16">
                     <a className="btn">Button</a>
-                </div> */}
+                </div>
             </div>
         </div>
+
     )
 }
 
