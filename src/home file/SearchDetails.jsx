19903c267093909d523a835952502b2905b5585a
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-21 07:18:43 -0800

    added file

---
+++ src/home file/SearchDetails.jsx
@@ -0,0 +1,29 @@
+
+function SearchDetails({ search }) {
+
+    const { id, image, name, paragraph } = search
+
+    return (
+        <div>
+
+            <div className="card bg-base-100 h-[500px] w-96 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img
+                        src={image}
+                        alt="Shoes"
+                        className="rounded-xl  h-[200px] w-[300px] " />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title dataText text-4xl"> {name} </h2>
+                    <p className=" text-gray-400 font-serif ">{paragraph}</p>
+                    <div className="card-actions">
+                        <button className="btn bg-cyan-700 text-white ">Learn More</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default SearchDetails
--- src/home file/SearchItems.jsx
+++ src/home file/SearchItems.jsx
@@ -1,21 +1,59 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import SearchDetails from "./SearchDetails"
 
 
 function SearchItems() {
 
+    const [search, setSearch] = useState([])
+
+    const [searchItems, setSearchItems] = useState("")
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        fetch('search.json')
+            .then(res => res.json())
+            .then(data => {
+                setSearch(data)
+                setItems(data)
+            })
+    }, [])
+
+    const handleSearch = event => {
+
+        const searchValue = event.target.value.toLowerCase();
+        setSearchItems(searchValue)
+
+        const searchData = search.filter(items => items.name.toLowerCase().includes(searchValue))
+        setItems(searchData)
+
+    }
+
     return (
-        <div className=" mt-20 mb-20 ml-16 mr-16">
-
+        <div className=" mb-20 ml-16 mr-16">
             <div>
-
-
-
-
-
-
-
-
+                <div>
+                    <p className=" text-black dataText text-center text-8xl">Search Items</p>
+                    <p className=" text-center text-xl text-gray-500 ">"In the age of information, ignorance is a choice."
+                        – Donny Miller</p>
+                </div>
+                <div className=" flex justify-center">
+                    <input
+                        className=" mb-10 mt-10 h-[60px] w-[1200px] border-[1px] border-gray-300 rounded-xl p-3 "
+                        placeholder="Search Items"
+                        value={searchItems}
+                        onChange={handleSearch}
+                        type="search"
+                        name="search" id="" />
+                </div>
+                <div className=" flex justify-center ">
+                    <div className=" grid  md:grid-cols-3 gap-5 ">
+                        {
+                            items.map(search => <SearchDetails key={search.id} search={search} ></SearchDetails>)
+                        }
+                    </div>
+                </div>
             </div>
-
         </div>
     )
 }
