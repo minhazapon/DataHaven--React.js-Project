a8c23d26a8424e3ea12f71038c946e599184690a
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-22 07:50:58 -0800

    added file

--- src/home file/Collection.jsx
+++ src/home file/Collection.jsx
@@ -63,6 +63,22 @@ function Collection() {
                 </div>
             </div>
 
+            {/* ===================================================================================================== */}
+
+            <div className=" mt-10 ">
+                <div className=" flex-col md:flex-row lg:flex-row flex justify-center items-center gap-10 ">
+                    <div>
+                        <img className="h-[300px] w-[500px] rounded-xl" src="https://www.coventry.ac.uk/globalassets/media/global/00-new-course-imagery/engineering-environment-and-computing/pg/management-of-information-systems-and-technology-1920x1080.jpg" alt="" />
+                    </div>
+                    <div>
+                        <p className=" text-center text-gray-500 font-thin ">An information paragraph is a concise piece of writing<br></br> that focuses on delivering clear, factual, and well-organized <br></br>details about a specific topic. It typically begins<br></br> with a topic sentence that introduces the main idea,<br></br> followed by supporting sentences that provide evidence,<br></br> examples, or explanations to expand on the topic. </p>
+                        <div className=" flex justify-center">
+                            <button className=" btn bg-blue-900 mt-5 text-white dataText text-2xl ">View All Courses</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
