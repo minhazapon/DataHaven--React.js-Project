562f87d494d5557678e6a304bd8a0381d2e04fc6
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-22 07:08:26 -0800

    added file

--- src/home file/OurPrograms.jsx
+++ src/home file/OurPrograms.jsx
@@ -1,22 +1,62 @@
 
-
 function OurPrograms() {
-
     return (
         <div className="mt-20 mb-20 ml-16 mr-16">
-
             <div>
-
-
-
-
-
-
-
+                <div>
+                    <p className="text-center dataText text-6xl text-black">Our Programs</p>
+                </div>
+                <div className=" flex justify-center mt-10">
+                    <div className=" grid  md:grid-cols-4 gap-5 ">
+                        <div className=" border-[1px] hover:border-cyan-400 p-5 ">
+                            <img className=" h-[60px] " src="https://cdn-icons-png.flaticon.com/128/53/53775.png" alt="" />
+                            <p className=" dataText text-4xl mt-5 ">Natural</p>
+                            <p className=" mt-5 font-sans text-gray-500 ">Lorem ipsum dolor sit <br></br>amet consectetur, adipisicing<br></br> elit. Sit esse magni deleniti</p>
+                        </div>
+                        <div className=" border-[1px] hover:border-cyan-400 p-5 ">
+                            <img className=" h-[60px] " src="https://cdn-icons-png.flaticon.com/128/3247/3247479.png" alt="" />
+                            <p className=" dataText text-4xl mt-5 ">information technology</p>
+                            <p className=" mt-5 font-sans text-gray-500 ">Lorem ipsum dolor sit <br></br>amet consectetur, adipisicing<br></br> elit. Sit esse magni deleniti</p>
+                        </div>
+                        <div className=" border-[1px] hover:border-cyan-400 p-5 ">
+                            <img className=" h-[60px] " src="https://cdn-icons-png.flaticon.com/128/763/763965.png" alt="" />
+                            <p className=" dataText text-4xl mt-5 ">Sports</p>
+                            <p className=" mt-5 font-sans text-gray-500 ">Lorem ipsum dolor sit <br></br>amet consectetur, adipisicing<br></br> elit. Sit esse magni deleniti</p>
+                        </div>
+                        <div className=" border-[1px] hover:border-cyan-400 p-5 ">
+                            <img className=" h-[60px] " src="https://cdn-icons-png.flaticon.com/128/8750/8750790.png" alt="" />
+                            <p className=" dataText text-4xl mt-5 ">Software</p>
+                            <p className=" mt-5 font-sans text-gray-500 ">Lorem ipsum dolor sit <br></br>amet consectetur, adipisicing<br></br> elit. Sit esse magni deleniti</p>
+                        </div>
+                        <div className=" border-[1px] hover:border-cyan-400 p-5 ">
+                            <img className=" h-[60px] " src="https://cdn-icons-png.flaticon.com/128/0/747.png" alt="" />
+                            <p className=" dataText text-4xl mt-5 ">Company</p>
+                            <p className=" mt-5 font-sans text-gray-500 ">Lorem ipsum dolor sit <br></br>amet consectetur, adipisicing<br></br> elit. Sit esse magni deleniti</p>
+                        </div>
+                        <div className=" border-[1px] hover:border-cyan-400 p-5 ">
+                            <img className=" h-[60px] " src="https://cdn-icons-png.flaticon.com/128/721/721671.png" alt="" />
+                            <p className=" dataText text-4xl mt-5 ">Programming Languages</p>
+                            <p className=" mt-5 font-sans text-gray-500 ">Lorem ipsum dolor sit <br></br>amet consectetur, adipisicing<br></br> elit. Sit esse magni deleniti</p>
+                        </div>
+                        <div className=" border-[1px] hover:border-cyan-400 p-5 ">
+                            <img className=" h-[60px] " src="https://cdn-icons-png.flaticon.com/128/10873/10873840.png" alt="" />
+                            <p className=" dataText text-4xl mt-5 ">River</p>
+                            <p className=" mt-5 font-sans text-gray-500 ">Lorem ipsum dolor sit <br></br>amet consectetur, adipisicing<br></br> elit. Sit esse magni deleniti</p>
+                        </div>
+                        <div className=" border-[1px] hover:border-cyan-400 p-5 ">
+                            <img className=" h-[60px] " src="https://cdn-icons-png.flaticon.com/128/53/53137.png" alt="" />
+                            <p className=" dataText text-4xl mt-5 ">Cities</p>
+                            <p className=" mt-5 font-sans text-gray-500 ">Lorem ipsum dolor sit <br></br>amet consectetur, adipisicing<br></br> elit. Sit esse magni deleniti</p>
+                        </div>
+                        <div className=" border-[1px] hover:border-cyan-400 p-5 ">
+                            <img className=" h-[60px] " src="https://cdn-icons-png.flaticon.com/128/15941/15941131.png" alt="" />
+                            <p className=" dataText text-4xl mt-5 ">Tourist places</p>
+                            <p className=" mt-5 font-sans text-gray-500 ">Lorem ipsum dolor sit <br></br>amet consectetur, adipisicing<br></br> elit. Sit esse magni deleniti</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         </div>
     )
 }
-
 export default OurPrograms
