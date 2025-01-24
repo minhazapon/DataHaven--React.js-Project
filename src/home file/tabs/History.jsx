942e289c2f5376176a6139e6048f405db77e2a8f
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-23 20:17:14 -0800

    added file

--- src/home file/tabs/History.jsx
+++ src/home file/tabs/History.jsx
@@ -4,33 +4,27 @@ function History() {
     return (
         <div className=" flex justify-center mt-10  ">
             <div className=" grid  md:grid-cols-2 gap-10 ">
-
-                <div className="h-[400px] p-6 rounded-md shadow-md bg-black text-gray-50">
+                <div className="h-[400px] p-6 rounded-md shadow-md bg-black text-gray-50  hover:border-[1px] border-cyan-400">
                     <img src="https://dualcreditathome.com/wp-content/uploads/2014/02/history.jpg" alt="" className="object-cover object-center w-full rounded-md h-[200px] bg-gray-500" />
                     <div className="mt-6 mb-2">
                         <span className="block text-xs font-medium tracking-widest uppercase text-white">History Part 1</span>
                     </div>
                     <p className="text-gray-100">History is the study of past events, particularly those involving human activity, societies, and civilizations.</p>
                 </div>
-
-
-                <div className="h-[400px] p-6 rounded-md shadow-md bg-black text-gray-50">
+                <div className="h-[400px] p-6 rounded-md shadow-md bg-black text-gray-50 hover:border-[1px] border-cyan-400">
                     <img src="https://poolegrammar.com/wp-content/uploads/Historical-Fiction-scaled.jpg" alt="" className="object-cover object-center w-full rounded-md h-[200px] bg-gray-500" />
                     <div className="mt-6 mb-2">
                         <span className="block text-xs font-medium tracking-widest uppercase text-white">History Part 2</span>
                     </div>
                     <p className="text-gray-100">History is the study of past events, particularly those involving human activity, societies, and civilizations.</p>
                 </div>
-
-
-                <div className="h-[400px] p-6 rounded-md shadow-md bg-black text-gray-50">
+                <div className="h-[400px] p-6 rounded-md shadow-md bg-black text-gray-50 hover:border-[1px] border-cyan-400">
                     <img src="https://thedispatch.blob.core.windows.net/thedispatchimages/2021/09/History.jpg" alt="" className="object-cover object-center w-full rounded-md h-[200px] bg-gray-500" />
                     <div className="mt-6 mb-2">
                         <span className="block text-xs font-medium tracking-widest uppercase text-white">History Part 3</span>
                     </div>
                     <p className="text-gray-100">History is the study of past events, particularly those involving human activity, societies, and civilizations.</p>
                 </div>
-
             </div>
         </div>
     )
--- src/home file/tabs/War.jsx
+++ src/home file/tabs/War.jsx
@@ -2,39 +2,28 @@
 
 function War() {
     return (
-        <div>
-            <div className=" flex justify-center mt-10  ">
-                <div className=" grid  md:grid-cols-2 gap-10 ">
-
-                    <div className="h-[400px] p-6 rounded-md shadow-md bg-black text-gray-50">
-                        <img src="https://source.unsplash.com/random/300x300/?1" alt="" className="object-cover object-center w-full rounded-md h-[200px] bg-gray-500" />
-                        <div className="mt-6 mb-2">
-                            <span className="block text-xs font-medium tracking-widest uppercase text-violet-400">Quisque</span>
-                            <h2 className="text-xl font-semibold tracking-wide">Nam maximus purus</h2>
-                        </div>
-                        <p className="text-gray-100">Mauris et lorem at elit tristique dignissim et ullamcorper elit. In sed feugiat mi. Etiam ut lacinia dui.</p>
+        <div className=" flex justify-center mt-10  ">
+            <div className=" grid  md:grid-cols-2 gap-10 ">
+                <div className="h-[400px] p-6 rounded-md shadow-md bg-black text-gray-50 hover:border-[1px] border-cyan-400">
+                    <img src="https://dualcreditathome.com/wp-content/uploads/2014/02/history.jpg" alt="" className="object-cover object-center w-full rounded-md h-[200px] bg-gray-500" />
+                    <div className="mt-6 mb-2">
+                        <span className="block text-xs font-medium tracking-widest uppercase text-white">History Part 1</span>
                     </div>
-
-
-                    <div className="h-[400px] p-6 rounded-md shadow-md bg-black text-gray-50">
-                        <img src="https://source.unsplash.com/random/300x300/?1" alt="" className="object-cover object-center w-full rounded-md h-[200px] bg-gray-500" />
-                        <div className="mt-6 mb-2">
-                            <span className="block text-xs font-medium tracking-widest uppercase text-violet-400">Quisque</span>
-                            <h2 className="text-xl font-semibold tracking-wide">Nam maximus purus</h2>
-                        </div>
-                        <p className="text-gray-100">Mauris et lorem at elit tristique dignissim et ullamcorper elit. In sed feugiat mi. Etiam ut lacinia dui.</p>
+                    <p className="text-gray-100">History is the study of past events, particularly those involving human activity, societies, and civilizations.</p>
+                </div>
+                <div className="h-[400px] p-6 rounded-md shadow-md bg-black text-gray-50 hover:border-[1px] border-cyan-400">
+                    <img src="https://poolegrammar.com/wp-content/uploads/Historical-Fiction-scaled.jpg" alt="" className="object-cover object-center w-full rounded-md h-[200px] bg-gray-500" />
+                    <div className="mt-6 mb-2">
+                        <span className="block text-xs font-medium tracking-widest uppercase text-white">History Part 2</span>
                     </div>
-
-
-                    <div className="h-[400px] p-6 rounded-md shadow-md bg-black text-gray-50">
-                        <img src="https://source.unsplash.com/random/300x300/?1" alt="" className="object-cover object-center w-full rounded-md h-[200px] bg-gray-500" />
-                        <div className="mt-6 mb-2">
-                            <span className="block text-xs font-medium tracking-widest uppercase text-violet-400">Quisque</span>
-                            <h2 className="text-xl font-semibold tracking-wide">Nam maximus purus</h2>
-                        </div>
-                        <p className="text-gray-100">Mauris et lorem at elit tristique dignissim et ullamcorper elit. In sed feugiat mi. Etiam ut lacinia dui.</p>
+                    <p className="text-gray-100">History is the study of past events, particularly those involving human activity, societies, and civilizations.</p>
+                </div>
+                <div className="h-[400px] p-6 rounded-md shadow-md bg-black text-gray-50 hover:border-[1px] border-cyan-400">
+                    <img src="https://thedispatch.blob.core.windows.net/thedispatchimages/2021/09/History.jpg" alt="" className="object-cover object-center w-full rounded-md h-[200px] bg-gray-500" />
+                    <div className="mt-6 mb-2">
+                        <span className="block text-xs font-medium tracking-widest uppercase text-white">History Part 3</span>
                     </div>
-
+                    <p className="text-gray-100">History is the study of past events, particularly those involving human activity, societies, and civilizations.</p>
                 </div>
             </div>
         </div>
