4a4725d067320808e9953edb2d38ac81086a63c8
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-23 21:04:51 -0800

    added file

--- src/home file/CountClock.jsx
+++ src/home file/CountClock.jsx
@@ -14,15 +14,12 @@ function Clock() {
             const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
             const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-
             return { days, hours, minutes, seconds };
-
         } else {
             return { days: 0, hours: 0, minutes: 0, seconds: 0 };
         }
 
     }
-
     const [clock, setClock] = useState(countClock())
 
     useEffect(() => {
