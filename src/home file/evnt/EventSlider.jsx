fd183a5a763e565d7ea033ea5d53a5b5c072a158
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-24 07:48:31 -0800

    added file

--- src/home file/evnt/EventSlider.jsx
+++ src/home file/evnt/EventSlider.jsx
@@ -9,11 +9,12 @@ import 'swiper/css/navigation';
 // import required modules
 import { Navigation } from 'swiper/modules';
 
+import { IoMdTime } from "react-icons/io";
+import { IoLocationOutline } from "react-icons/io5";
+
 function EventSlider() {
-
     return (
-        <div>
-
+        <div className=' flex-col md:flex-row lg:flex-row flex justify-center items-center gap-10 '>
             <>
                 <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
                     <SwiperSlide>
@@ -51,6 +52,77 @@ function EventSlider() {
                     </SwiperSlide>
                 </Swiper>
             </>
+
+            <div>
+                <div>
+                    <div className=" bg-[#48CFCB] w-fit md:w-[500px] p-10 rounded-xl text-white ">
+                        <p className=" text-4xl font-semibold font-sans ">Events</p>
+                        <div className=" mt-10 flex items-center gap-5 ">
+                            <div className=" border-[1px] border-white w-fit p-2 rounded-xl ">
+                                <p className=" text-xl text-center font-semibold ">23</p>
+                                <p className=" text-center font-semibold ">DEC</p>
+                            </div>
+                            <div>
+                                <p className=" font-sans text-2xl ">Learn to Write Flash Fiction</p>
+                                <div className=" mt-2 flex items-center gap-2 ">
+                                    <div className=" flex items-center gap-1">
+                                        <p><IoMdTime className=" h-[20px] w-[20px] " /></p>
+                                        <p className=" text-xs ">12:00 am - 5:00 pm</p>
+                                    </div>
+                                    <div className=" flex items-center gap-1">
+                                        <p><IoLocationOutline className=" h-[20px] w-[20px] " /></p>
+                                        <p className=" text-xs"> Birmingham, UK</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className=" mt-5 ">
+                            <hr></hr>
+                        </div>
+                        <div className=" mt-10 flex items-center gap-5 ">
+                            <div className=" border-[1px] border-white w-fit p-2 rounded-xl ">
+                                <p className=" text-xl text-center font-semibold ">15</p>
+                                <p className=" text-center font-semibold ">DEC</p>
+                            </div>
+                            <div>
+                                <p className=" font-sans text-2xl ">Data Science and Fake News</p>
+                                <div className=" mt-2 flex items-center gap-2 ">
+                                    <div className=" flex items-center gap-1">
+                                        <p><IoMdTime className=" h-[20px] w-[20px] " /></p>
+                                        <p className=" text-xs ">12:00 am - 5:00 pm</p>
+                                    </div>
+                                    <div className=" flex items-center gap-1">
+                                        <p><IoLocationOutline className=" h-[20px] w-[20px] " /></p>
+                                        <p className=" text-xs">  Venice, Italy</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className=" mt-5 ">
+                            <hr></hr>
+                        </div>
+                        <div className=" mt-10 flex items-center gap-5 ">
+                            <div className=" border-[1px] border-white w-fit p-2 rounded-xl ">
+                                <p className=" text-xl text-center font-semibold ">10</p>
+                                <p className=" text-center font-semibold ">DEC</p>
+                            </div>
+                            <div>
+                                <p className=" font-sans text-2xl ">Summer School 2022</p>
+                                <div className=" mt-2 flex items-center gap-2 ">
+                                    <div className=" flex items-center gap-1">
+                                        <p><IoMdTime className=" h-[20px] w-[20px] " /></p>
+                                        <p className=" text-xs ">12:00 am - 5:00 pm</p>
+                                    </div>
+                                    <div className=" flex items-center gap-1">
+                                        <p><IoLocationOutline className=" h-[20px] w-[20px] " /></p>
+                                        <p className=" text-xs"> NewYork, USA</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
--- src/home file/evnt/Events.jsx
+++ src/home file/evnt/Events.jsx
@@ -1,27 +1,15 @@
 import EventSlider from "./EventSlider"
-
+import { IoMdTime } from "react-icons/io";
+import { IoLocationOutline } from "react-icons/io5";
 
 function Events() {
-
-
     return (
         <div>
-
-            <div className="mt-20 mb-20 ml-20 mr-20">
-
+            <div className="mt-20 mb-20 ml-20 mr-20  ">
                 <div>
                     <EventSlider></EventSlider>
                 </div>
-
-                <div>
-
-
-
-                </div>
-
-
             </div>
-
         </div>
     )
 }
