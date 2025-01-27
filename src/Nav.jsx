0681d2ee8833b319f028ed4f482625ad34de8f11
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-27 07:43:38 -0800

    added file

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,23 +1,41 @@
-import { NavLink } from "react-router-dom"
+import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 function Nav() {
+    const [darkMode, setDarkMode] = useState(false);
 
-    const NavZ = <>
-        <li className="text-xs font-thin"><NavLink to='/'>Home</NavLink></li>
-        <li className="text-xs font-thin"><NavLink to='/add'>Add Information</NavLink></li>
-        <li className="text-xs font-thin"><NavLink to='/service'>Service</NavLink></li>
-        <li className="text-xs font-thin"><NavLink to='/contact'>Contact</NavLink></li>
-    </>
+    // Toggle dark mode class on the body
+    useEffect(() => {
+        if (darkMode) {
+            document.body.classList.add("dark");
+        } else {
+            document.body.classList.remove("dark");
+        }
+    }, [darkMode]);
+
+    const NavZ = (
+        <>
+            <li className="text-xs font-thin">
+                <NavLink to="/">Home</NavLink>
+            </li>
+            <li className="text-xs font-thin">
+                <NavLink to="/add">Add Information</NavLink>
+            </li>
+            <li className="text-xs font-thin">
+                <NavLink to="/service">Service</NavLink>
+            </li>
+            <li className="text-xs font-thin">
+                <NavLink to="/contact">Contact</NavLink>
+            </li>
+        </>
+    );
 
     return (
         <div>
             <div className="navbar bg-base-100 shadow p-5">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <button
-                            tabIndex={0}
-                            className="btn btn-ghost lg:hidden"
-                        >
+                        <button tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 className="h-6 w-6"
@@ -56,13 +74,21 @@ function Nav() {
                     <ul className="menu menu-horizontal px-1">{NavZ}</ul>
                 </div>
 
-                <div className="navbar-end mr-4 lg:mr-16">
+                <div className="navbar-end flex items-center gap-4 mr-4 lg:mr-16">
+                    <div
+                        className="relative w-12 h-6 flex items-center bg-gray-300 dark:bg-gray-700 rounded-full cursor-pointer"
+                        onClick={() => setDarkMode(!darkMode)}
+                    >
+                        <div
+                            className={`absolute w-6 h-6 bg-white dark:bg-gray-900 rounded-full shadow-md transform duration-300 ${darkMode ? "translate-x-6" : "translate-x-0"
+                                }`}
+                        ></div>
+                    </div>
                     <a className="btn dataText bg-cyan-600 text-white text-2xl">Sign In</a>
                 </div>
             </div>
         </div>
-
-    )
+    );
 }
 
-export default Nav
+export default Nav;
