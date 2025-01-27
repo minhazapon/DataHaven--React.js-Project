cf6ef4292dfc9d33d69f701cc7c21516b3517b9c
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-27 07:12:23 -0800

    added file

--- src/service file/ServiceCard.jsx
+++ src/service file/ServiceCard.jsx
@@ -23,132 +23,11 @@ function ServiceCard() {
                                     alt="Hero Image"
                                 />
                                 <div>
-                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                    <p className="py-6 text-black">
-                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                    <h1 className="text-6xl font-thin dataText">Natural</h1>
+                                    <p className="py-6 text-black font-mono">
+                                        This is a detailed description of item one. It provides key features and benefits of the item.
                                     </p>
-                                    <button className="btn btn-primary">Get Started</button>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
-                            <div className="hero-content flex-col lg:flex-row">
-                                <img
-                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
-                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
-                                    alt="Hero Image"
-                                />
-                                <div>
-                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                    <p className="py-6 text-black">
-                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                                    </p>
-                                    <button className="btn btn-primary">Get Started</button>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
-                            <div className="hero-content flex-col lg:flex-row">
-                                <img
-                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
-                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
-                                    alt="Hero Image"
-                                />
-                                <div>
-                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                    <p className="py-6 text-black">
-                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                                    </p>
-                                    <button className="btn btn-primary">Get Started</button>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
-                            <div className="hero-content flex-col lg:flex-row">
-                                <img
-                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
-                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
-                                    alt="Hero Image"
-                                />
-                                <div>
-                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                    <p className="py-6 text-black">
-                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                                    </p>
-                                    <button className="btn btn-primary">Get Started</button>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
-                            <div className="hero-content flex-col lg:flex-row">
-                                <img
-                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
-                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
-                                    alt="Hero Image"
-                                />
-                                <div>
-                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                    <p className="py-6 text-black">
-                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                                    </p>
-                                    <button className="btn btn-primary">Get Started</button>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
-                            <div className="hero-content flex-col lg:flex-row">
-                                <img
-                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
-                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
-                                    alt="Hero Image"
-                                />
-                                <div>
-                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                    <p className="py-6 text-black">
-                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                                    </p>
-                                    <button className="btn btn-primary">Get Started</button>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
-                            <div className="hero-content flex-col lg:flex-row">
-                                <img
-                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
-                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
-                                    alt="Hero Image"
-                                />
-                                <div>
-                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                    <p className="py-6 text-black">
-                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                                    </p>
-                                    <button className="btn btn-primary">Get Started</button>
+                                    <button className="btn bg-cyan-900 text-white ">Search</button>
                                 </div>
                             </div>
                         </div>
@@ -157,37 +36,142 @@ function ServiceCard() {
                         <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
                             <div className="hero-content flex-col lg:flex-row">
                                 <img
-                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
+                                    src="https://images.yourstory.com/cs/1/b3cc3444ab5e11e88691f70342131e20/Whatissoftwareandtypesofsoftwarewithexamples1575272423828jpg?mode=crop&crop=faces&ar=16%3A9&format=auto&w=1920&q=75"
                                     className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
                                     alt="Hero Image"
                                 />
                                 <div>
-                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                    <p className="py-6 text-black">
-                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                    <h1 className="text-6xl font-thin dataText">Software</h1>
+                                    <p className="py-6 text-black font-mono">
+                                        This is a detailed description of item one. It provides key features and benefits of the item.
                                     </p>
-                                    <button className="btn btn-primary">Get Started</button>
+                                    <button className="btn bg-cyan-900 text-white ">Search</button>
                                 </div>
                             </div>
                         </div>
 
 
-
                         <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
                             <div className="hero-content flex-col lg:flex-row">
                                 <img
-                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
+                                    src="https://www.rebeccavanommen.ph/wp-content/uploads/2019/10/01-Night-Run-04-layout_edit_WLogo.jpg"
                                     className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
                                     alt="Hero Image"
                                 />
                                 <div>
-                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                    <p className="py-6 text-black">
-                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                    <h1 className="text-6xl font-thin dataText">Sports</h1>
+                                    <p className="py-6 text-black font-mono">
+                                        This is a detailed description of item one. It provides key features and benefits of the item.
                                     </p>
-                                    <button className="btn btn-primary">Get Started</button>
+                                    <button className="btn bg-cyan-900 text-white ">Search</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <img
+                                    src="https://ccitraining.edu/wp-content/uploads/2023/12/Information-Technology.jpg"
+                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
+                                    alt="Hero Image"
+                                />
+                                <div>
+                                    <h1 className="text-6xl font-thin dataText">Information Technology</h1>
+                                    <p className="py-6 text-black font-mono">
+                                        This is a detailed description of item one. It provides key features and benefits of the item.
+                                    </p>
+                                    <button className="btn bg-cyan-900 text-white ">Search</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <img
+                                    src="https://www.bankrate.com/2022/07/25160346/Most-valuable-tech-companies.jpg?auto=webp&optimize=high&crop=16:9"
+                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
+                                    alt="Hero Image"
+                                />
+                                <div>
+                                    <h1 className="text-6xl font-thin dataText">Company</h1>
+                                    <p className="py-6 text-black font-mono">
+                                        This is a detailed description of item one. It provides key features and benefits of the item.
+                                    </p>
+                                    <button className="btn bg-cyan-900 text-white ">Search</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <img
+                                    src="https://media.licdn.com/dms/image/v2/D4D12AQExd0s7g9AxpQ/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1703166341281?e=2147483647&v=beta&t=6FY186NBgvVVlmCtZ5O3pa6DhK-seHvfeKHaYQXwVsg"
+                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
+                                    alt="Hero Image"
+                                />
+                                <div>
+                                    <h1 className="text-6xl font-thin dataText">Programming languages</h1>
+                                    <p className="py-6 text-black font-mono">
+                                        This is a detailed description of item one. It provides key features and benefits of the item.
+                                    </p>
+                                    <button className="btn bg-cyan-900 text-white ">Search</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <img
+                                    src="https://ecdn.dhakatribune.net/contents/cache/images/640x359x1/uploads/media/2024/09/01/River_Opu-6457ccd8884e8dfeaf76951f8c182f4a.jpg?jadewits_media_id=27657"
+                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
+                                    alt="Hero Image"
+                                />
+                                <div>
+                                    <h1 className="text-6xl font-thin dataText">River</h1>
+                                    <p className="py-6 text-black font-mono">
+                                        This is a detailed description of item one. It provides key features and benefits of the item.
+                                    </p>
+                                    <button className="btn bg-cyan-900 text-white ">Search</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <img
+                                    src="https://imageio.forbes.com/specials-images/imageserve/66a287879f9e730526b6f792/Tokyo-city-in-Japan/960x0.jpg?format=jpg&width=960"
+                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
+                                    alt="Hero Image"
+                                />
+                                <div>
+                                    <h1 className="text-6xl font-thin dataText">Cities</h1>
+                                    <p className="py-6 text-black font-mono">
+                                        This is a detailed description of item one. It provides key features and benefits of the item.
+                                    </p>
+                                    <button className="btn bg-cyan-900 text-white ">Search</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <img
+                                    src="https://s7ap1.scene7.com/is/image/incredibleindia/The-Best-Adventure-Experiences-in-Manali1-hero?qlt=82&ts=1726731002736"
+                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
+                                    alt="Hero Image"
+                                />
+                                <div>
+                                    <h1 className="text-6xl font-thin dataText">Tourist places</h1>
+                                    <p className="py-6 text-black font-mono">
+                                        This is a detailed description of item one. It provides key features and benefits of the item.
+                                    </p>
+                                    <button className="btn bg-cyan-900 text-white ">Search</button>
                                 </div>
                             </div>
                         </div>
