cf20e28941bfab602301dc6b8df99f4eefc636aa
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-27 06:51:04 -0800

    added file

--- src/service file/ServiceCard.jsx
+++ src/service file/ServiceCard.jsx
@@ -6,30 +6,200 @@ function ServiceCard() {
 
             <div>
 
-                <div>
+                <div className=" flex justify-center items-center gap-2 ">
+                    <img className=" h-[60px] " src="https://cdn-icons-png.flaticon.com/128/9610/9610533.png" alt="" />
                     <p className=" text-center text-6xl font-sans font-bold text-black ">Our Information Service Category</p>
                 </div>
 
 
-                <div>
-
+                <div className=" mt-10 ">
                     <div>
+
+                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <img
+                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
+                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
+                                    alt="Hero Image"
+                                />
+                                <div>
+                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                                    <p className="py-6 text-black">
+                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                    </p>
+                                    <button className="btn btn-primary">Get Started</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <img
+                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
+                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
+                                    alt="Hero Image"
+                                />
+                                <div>
+                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                                    <p className="py-6 text-black">
+                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                    </p>
+                                    <button className="btn btn-primary">Get Started</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <img
+                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
+                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
+                                    alt="Hero Image"
+                                />
+                                <div>
+                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                                    <p className="py-6 text-black">
+                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                    </p>
+                                    <button className="btn btn-primary">Get Started</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <img
+                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
+                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
+                                    alt="Hero Image"
+                                />
+                                <div>
+                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                                    <p className="py-6 text-black">
+                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                    </p>
+                                    <button className="btn btn-primary">Get Started</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <img
+                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
+                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
+                                    alt="Hero Image"
+                                />
+                                <div>
+                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                                    <p className="py-6 text-black">
+                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                    </p>
+                                    <button className="btn btn-primary">Get Started</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <img
+                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
+                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
+                                    alt="Hero Image"
+                                />
+                                <div>
+                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                                    <p className="py-6 text-black">
+                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                    </p>
+                                    <button className="btn btn-primary">Get Started</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <img
+                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
+                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
+                                    alt="Hero Image"
+                                />
+                                <div>
+                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                                    <p className="py-6 text-black">
+                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                    </p>
+                                    <button className="btn btn-primary">Get Started</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <img
+                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
+                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
+                                    alt="Hero Image"
+                                />
+                                <div>
+                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                                    <p className="py-6 text-black">
+                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                    </p>
+                                    <button className="btn btn-primary">Get Started</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div className="hero bg-gradient-to-r from-white via-gray-200 to-cyan-800">
+                            <div className="hero-content flex-col lg:flex-row">
+                                <img
+                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1200px-24701-nature-natural-beauty.jpg"
+                                    className="max-w-sm rounded-lg shadow-2xl h-[200px] w-[300px]"
+                                    alt="Hero Image"
+                                />
+                                <div>
+                                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                                    <p className="py-6 text-black">
+                                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                    </p>
+                                    <button className="btn btn-primary">Get Started</button>
+                                </div>
+                            </div>
+                        </div>
 
 
 
 
 
                     </div>
-
-
                 </div>
 
-
-
-
-
             </div>
-
         </div>
     )
 }
