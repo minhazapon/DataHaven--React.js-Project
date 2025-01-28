cb1dc793bd931255848b7ed8c08689a37ae26460
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-28 06:53:21 -0800

    added file

--- src/contact file/ContactBanner.jsx
+++ src/contact file/ContactBanner.jsx
@@ -3,7 +3,10 @@
 function ContactBanner() {
     return (
         <div>
-
+            <div>
+                <p className=" text-center dataText text-7xl font-bold ">Contact Wit Us</p>
+                <p className=" text-center mt-5 font-mono ">Fantastic LMS and instructors, well laid out, good speed, and explains.</p>
+            </div>
         </div>
     )
 }
