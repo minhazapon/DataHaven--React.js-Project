6c71c7015003f7c72c610c49907201d93e46a77f
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-28 07:06:53 -0800

    added file

--- src/contact file/ContactBanner.jsx
+++ src/contact file/ContactBanner.jsx
@@ -2,8 +2,10 @@
 
 function ContactBanner() {
     return (
-        <div>
-            <div>
+        <div className=" bg-no-repeat bg-cover " style={{
+            backgroundImage: "url(https://img.freepik.com/free-vector/blue-copy-space-digital-background_23-2148821698.jpg?t=st=1738076369~exp=1738079969~hmac=7a73fb9ba8897661598b9672b768b922035f75e90c8c612c531d0f5ab26601e5&w=740)",
+        }}>
+            <div className=" text-white bg-gradient-to-r from-[#0000003a] via-[#020202cb] p-10 ">
                 <p className=" text-center dataText text-7xl font-bold ">Contact Wit Us</p>
                 <p className=" text-center mt-5 font-mono ">Fantastic LMS and instructors, well laid out, good speed, and explains.</p>
             </div>
