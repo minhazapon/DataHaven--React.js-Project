cf6db6ca44b1a28abc08f622a25ab306aa6e2749
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-30 21:08:32 -0800

    added file

--- src/SearchItems file/AllSearchItems.jsx
+++ src/SearchItems file/AllSearchItems.jsx
@@ -1,22 +1,15 @@
 import { useLoaderData } from "react-router-dom"
 import DetailsOfItems from "./DetailsOfItems"
 
-
-
 function AllSearchItems() {
-
     const loader = useLoaderData()
-
     return (
         <div className=" mt-20 mb-20 ml-20 mr-20 ">
-
             <div>
-
                 <div className=" flex justify-center items-center gap-3 ">
                     <img className="  h-[20px] md:h-[60px] " src="https://cdn-icons-png.flaticon.com/128/16207/16207051.png" alt="" />
                     <p className=" dataText  text-2xl md:text-6xl text-black ">Search Hub</p>
                 </div>
-
                 <div>
                     <div>
                         {
@@ -24,7 +17,6 @@ function AllSearchItems() {
                         }
                     </div>
                 </div>
-
             </div>
         </div>
     )
--- src/SearchItems file/DetailsOfItems.jsx
+++ src/SearchItems file/DetailsOfItems.jsx
@@ -1,15 +1,8 @@
 
-// "image": "https://upload.wikimedia.org/wikipedia/commons/6/6b/Save_the_sundarbans_20.jpg",
-//     "name": "Sundarbans",
-//         "title": "Largest Mangrove Forest",
-//             "details": "Sundarbans is the largest mangrove forest in the world and home to the Royal Bengal Tiger.",
-//                 "location": "21.9497° N, 89.1833° E",
-//                     "countryName": "Bangladesh",
-//                         "placeName": "
 
 function DetailsOfItems({ loader }) {
 
-    const { id, } = loader
+    const { id, image, name, title, details, location, countryName, placeName } = loader
 
     return (
         <div>
